72da1ddc3504186de7a9f5a467e7d02f15e1c715
MorganWilliamson <mwilliamson7695@gmail.com>
2021-06-28 15:27:27 -0500

    Added functionality for sticky navbar.

---
+++ portfolio/index.js
@@ -0,0 +1,13 @@
+// Sticky Navbar
+const nav = document.querySelector('#navbar');
+let navTop = nav.offsetTop;
+
+function fixedNav() {
+  if (window.scrollY >= navTop) {    
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');    
+  }
+}
+
+window.addEventListener('scroll', fixedNav);
