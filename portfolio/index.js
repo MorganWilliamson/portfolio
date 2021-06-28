d93634943c1fd20ef34202ac63de76c6e424064d
MorganWilliamson <mwilliamson7695@gmail.com>
2021-06-28 18:18:41 -0500

    Reworking contact form. Setting up JS.

--- portfolio/index.js
+++ portfolio/index.js
@@ -11,3 +11,29 @@ function fixedNav() {
 }
 
 window.addEventListener('scroll', fixedNav);
+
+///// Contact Form /////
+const form = document.getElementById("contact-form");
+
+// Event handler
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let mail = new FormData(form);
+
+  sendMail(mail);
+})
+
+const sendMail = (mail) => {
+  // base url
+  fetch("url goes here", {
+    method: "post", // POST data to the server
+    body: mail, // mail, because we're sending data
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
